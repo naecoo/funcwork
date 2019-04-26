ea6823a841c27c4f3316e8308c4a7e2816514841
naeco <847185911@qq.com>
2019-04-26 22:32:13 +0800

    update build config

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import babel from 'rollup-plugin-babel' 
+import { terser } from 'rollup-plugin-terser'
+
+const config = {
+  input: 'src/index.js',
+  output: [{
+    file: 'dist/funcwork.es.js',
+    format: 'es'
+  }, {
+    file: 'dist/funcwork.umd.js',
+    format: 'umd',
+    name: 'funcworker'
+  }],
+  plugins: [
+    babel({
+      exclude: '**/node_modules/**',
+      runtimeHelpers: true
+    }),
+    terser({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      }
+    })
+  ]
+}
+
+export default config
