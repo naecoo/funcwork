780818a7b38a64c171906672bff4e748de152847
naecoo <ocean7hy@gmail.com>
2022-05-24 09:47:17 +0800

    chore: refactor build script

--- build.js
+++ build.js
@@ -1,13 +1,7 @@
 const { resolve } = require('path');
 const { build, buildSync } = require('esbuild');
 
-const formats = {
-  'cjs': 'index.js',
-  'esm': 'index.esm.js',
-  'iife': 'index.iife.js'
-}
-
-const buildWorker = () => {
+const buildWorkerScript = () => {
   const result = buildSync({
     format: 'iife',
     entryPoints: [resolve(__dirname, './src/worker.ts')],
@@ -19,33 +13,42 @@ const buildWorker = () => {
       '.ts': 'ts'
     },
     tsconfig: resolve(__dirname, './tsconfig.json')
-  })
-  return Buffer.from(result.outputFiles[0].contents).toString('utf-8')
+  });
+  return Buffer.from(result.outputFiles[0].contents).toString('utf-8');
 };
 
-const workerScriptContent = JSON.stringify(buildWorker());
-module.exports = Object.keys(formats).map(format => {
-  const fileName = formats[format]
-  build({
-    format,
-    globalName: 'funcWork',
-    entryPoints: [resolve(__dirname, './src/index.ts')],
-    outfile: resolve(__dirname, './dist/', fileName),
-    bundle: true,
-    minify: true,
-    sourcemap: true,
-    platform: 'browser',
-    loader: {
-      '.ts': 'ts'
-    },
-    define: {
-      '__WORKER_SCRIPT__': workerScriptContent
-    },
-    tsconfig: resolve(__dirname, './tsconfig.json')
-  }).then(() => {
-    console.info(`— ${fileName} was built`)
-  }).catch((e) => {
-    console.info(`🚨 ${fileName} build error:`)
-    console.error(e);
+const run = () => {
+  const workerScriptContent = JSON.stringify(buildWorkerScript());
+
+  const formats = {
+    'cjs': 'index.js',
+    'esm': 'index.esm.js',
+    'iife': 'index.iife.js'
+  };
+
+  Object.keys(formats).forEach(format => {
+    const fileName = formats[format];
+    build({
+      format,
+      globalName: 'funcWork',
+      entryPoints: [resolve(__dirname, './src/index.ts')],
+      outfile: resolve(__dirname, './dist/', fileName),
+      bundle: true,
+      minify: true,
+      sourcemap: true,
+      platform: 'browser',
+      loader: {
+        '.ts': 'ts'
+      },
+      define: {
+        '__WORKER_SCRIPT__': workerScriptContent
+      },
+      tsconfig: resolve(__dirname, './tsconfig.json')
+    }).then(() => {
+      console.info(`— ${fileName} was built`);
+    }).catch((e) => {
+      console.info(`🚨 ${fileName} build error:`);
+      console.error(e);
+    });
   })
-})
+};
